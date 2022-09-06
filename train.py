c5f52a858b857469ffb81516f7b9433509339cc2
Marco Mameli <marcomameli@marcomameli01-minimac.local>
2022-09-06 22:58:19 +0200

    Adding HSV to RGB for image generated to neptune

--- train.py
+++ train.py
@@ -118,7 +118,7 @@ for epoch in range(s_epoch, args.epochs):
                 fake_pillow = image_transform(fake_generated[n].cpu())
                 real_pillow = image_transform(data['cycles'][n].cpu())
 
-                run["fake_generated_epoch_" + str(epoch) + "_batch_" + str(i)].log(fake_pillow)
+                run["fake_generated_epoch_" + str(epoch) + "_batch_" + str(i)].log(fake_pillow.convert(mode='RGB'))
                 run["real_image_epoch_" + str(epoch) + "_batch_" + str(i)].log(real_pillow)
 
 
