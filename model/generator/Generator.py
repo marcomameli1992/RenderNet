b19488492a106f5ddaa795c09ffe03154247a6a3
Marco Mameli <marcomameli@marcomameli01-minimac.local>
2022-09-20 23:13:21 +0200

    Added the selection for the encoder configuration

--- model/generator/Generator.py
+++ model/generator/Generator.py
@@ -6,11 +6,11 @@ from torchvision import ops as OPS
 
 class Generator(NN.Module):
 
-        def __init__(self, in_channels, out_channels):
+        def __init__(self, in_channels, out_channels, use_all=True, use_albedo=False, use_depth=False, use_emissive=False, use_metalness=False, use_normal=False, use_roughness=False, use_position=False):
 
             super(Generator, self).__init__()
 
-            self.encoder = Encoder()
+            self.encoder = Encoder(use_all, use_albedo, use_depth, use_emissive, use_metalness, use_normal, use_roughness, use_position)
             self.squeeze_and_excitation_layer = OPS.SqueezeExcitation(input_channels=7680, squeeze_channels=7680, activation=NN.SiLU)
             self.decoder = Decoder(in_channels, out_channels)
 
