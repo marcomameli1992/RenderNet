97394a4cd0177a614bf6c7aa967398441ef0a4fb
Marco Mameli <marcomameli@marcomameli01-minimac.local>
2022-10-07 20:31:34 +0200

    fix gpu problem

--- train2.py
+++ train2.py
@@ -117,8 +117,7 @@ def calc_gradient_penalty(netD, real_data, fake_data):
     disc_interpolates = netD(interpolates)
 
     gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
-                              grad_outputs=torch.ones(disc_interpolates.size()).cuda(gpu) if use_cuda else torch.ones(
-                                  disc_interpolates.size()),
+                              grad_outputs=torch.ones(disc_interpolates.size()).to(device),
                               create_graph=True, retain_graph=True, only_inputs=True)[0]
     gradients = gradients.view(gradients.size(0), -1)
 
